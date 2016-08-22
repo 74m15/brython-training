d5b6d08f7e88eff26705d331a4966ebd928410e6
Pasquale Capuano <p.capuano@macbook-raffaella.local>
2016-08-22 02:29:26 +0200

    Done pure JavaScript Web Worker

--- mandel.js
+++ mandel.js
@@ -1,18 +1,45 @@
-function mandel(cr, ci, max_iter) {
+function mandel(width, max_iter) {
 
-  zr = cr;
-  zi = ci;
-  
-  for (i = 1; i < max_iter; i++) {
-    tmp = zr;
-    zr = zr*zr - zi*zi + cr;
-    zi = 2 * tmp * zi + ci;
-    
-    if (zr * zr + zi * zi > 4)
-      return max(0, 256 - i);
+  f = 2.5 / width;
+
+  for (y = 0; y < width; y++) {
+    for(x = 0; x < width; x++) {
+      cr = -2.0 + x * f;
+      ci = 1.25 - y * f;
+
+      zr = cr;
+      zi = ci;
+      color = 0;
+
+      for (i = 1; i < max_iter; i++) {
+        tmp = zr;
+        zr = zr*zr - zi*zi + cr;
+        zi = 2 * tmp * zi + ci;
+        
+        if (zr * zr + zi * zi > 4) {
+          color = Math.max(0, 256 - i);
+
+          break;
+        }
+      }
+      
+      postMessage({ 'x':x, 'y':y, 'c':color });
+    }
   }
-  
-  return 0;
 }
 
-postMessage(mandel(0, 0, 50))
+addEventListener('message', function(e) {
+  var data = e.data;
+  
+  switch (data.cmd) {
+    case 'start':
+      console.log("Worker started!"); 
+      console.log("width=" + data.width);
+      console.log("max_iter=" + data.max_iter);
+
+      mandel(data.width, data.max_iter);
+      break;
+    case 'stop':
+      close();
+  }
+}, false);
